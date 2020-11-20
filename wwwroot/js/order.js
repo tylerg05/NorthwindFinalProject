6f1428e2597789a59c20d1fe42518e56f14e5b34
tylerg05 <tylergardella05@gmail.com>
2020-11-19 19:51:29 -0600

    Order.json

--- wwwroot/js/order.js
+++ wwwroot/js/order.js
@@ -7,6 +7,16 @@
             url: "../../api/order" + id,
             success: function (response, textStatus, jqXhr) {
                 console.log(response);
+                $('#order_row').html("");
+                for (var i = 0; i < response.length; i++) {
+                    var row = "<tr" + css + " data-id=\"" + response[i].orderId + "\" data-order-date=\"" + response[i].orderDate + "\" data-required-date=\"" + response[i].requiredDate +"\" data-order-date=\"" + response[i].shippedDate + "\">"
+                        + "<td>" + response[i].orderId + "</td>"
+                        + "<td class=\"text-right\">$" + response[i].orderDate + "</td>"
+                        + "<td class=\"text-right\">" + response[i].requiredDate + "</td>"
+                        + "<td class=\"text-right\">" + response[i].shippedDate + "</td>"
+                        + "</tr>";
+                    $('#order_row').append(row);
+                }
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 // log the error to the console
