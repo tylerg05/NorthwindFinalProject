7ea0039a91b303485966539f54874630942a3744
tylerg05 <tylergardella05@gmail.com>
2020-11-19 18:41:32 -0600

    Order Controller / View

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Controllers
+{
+    public class OrderController : Controller
+    {
+        private INorthwindRepository repository;
+        public OrderController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Index() => View();
+    }
+}
