d52601e50da1bb2d691f1fe52ef2e3b86ae3437e
tylerg05 <tylergardella05@gmail.com>
2020-11-28 15:53:47 -0600

    Order.js 1

--- wwwroot/js/order.js
+++ wwwroot/js/order.js
@@ -1,29 +1,14 @@
 ﻿$(function () {
-    $("#Unshipped").change(() => {
-        $('#order_rows').html("");
-        if (this.checked) {
-            getUnshippedOrders();
-        }
-        else {
-            getOrders();
-        }
-    });
+    getOrders();
 
     function getOrders() {
-        var id = $('#order_rows').data('id');
+        var unshipped = $('#Unshipped').prop('checked') ? "" : "/unshipped";
         $.getJSON({
-            url: "../../api/order" + id,
+            url: "../../api/order" + unshipped,
             success: function (response, textStatus, jqXhr) {
-                console.log(response);
+                //console.log(response);
                 $('#order_rows').html("");
-                for (var i = 0; i < response.length; i++) { /*
-                    var css;
-                    if (response[i].shippedDate == null) {
-                        css = " class=\"unshipped\"";
-                    }
-                    else {
-                        css = "";
-                    }*/
+                for (var i = 0; i < response.length; i++) { 
                     var row = "<tr" + " data-id=\"" + response[i].orderId + "\" data-order-date=\"" + response[i].orderDate + "\" data-required-date=\"" + response[i].requiredDate + "\" data-shipped-date=\"" + response[i].shippedDate + "\">"
                         + "<td class=\"text-right\">" + "</td>"
                         + "<td class=\"text-right\">" + response[i].orderId + "</td>"
@@ -41,27 +26,7 @@
         });
     };
 
-    function getUnshippedOrders() {
-        $.getJSON({
-            url: "../../api/order/unshipped",
-            success: function (response, textStatus, jqXhr) {
-                console.log(response);
-                $('#order_rows').html("");
-                for (var i = 0; i < response.length; i++) {
-                    var row = "<tr class=\"unshipped\"" + " data-id=\"" + response[i].orderId + "\" data-order-date=\"" + response[i].orderDate + "\" data-required-date=\"" + response[i].requiredDate + "\" data-shipped-date=\"" + response[i].shippedDate + "\">"
-                        + "<td class=\"text-right\">" + "</td>"
-                        + "<td class=\"text-right\">" + response[i].orderId + "</td>"
-                        + "<td class=\"text-right\">" + response[i].orderDate + "</td>"
-                        + "<td class=\"text-right\">" + response[i].requiredDate + "</td>"
-                        + "<td class=\"text-right\">" + "Unshipped" + "</td>"
-                        + "</tr>";
-                    $('#order_rows').append(row);
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                // log the error to the console
-                console.log("The following error occured: " + textStatus, errorThrown);
-            }
-        });
-    }
+    $('#Unchecked').on('change', function () {
+        getOrders();
+    });
 });
