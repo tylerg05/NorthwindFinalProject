931d5da85287841bf9d1d5e4b65fb2f3ca1f7f54
tylerg05 <tylergardella05@gmail.com>
2020-12-02 00:56:02 -0600

    Order.js Finished

--- wwwroot/js/order.js
+++ wwwroot/js/order.js
@@ -1,36 +1,36 @@
-﻿/*
-$(function () {
+﻿$(function () {
     getOrders()
 
     function getOrders() {
         var id = $('#order_rows').data('id');
-        var unshipped;
-        if ($('#Unshipped').prop('checked')) {
-            unshipped = "/unshipped";
-        }
-        else {
-            unshipped = "";
-        }
+        var unshipped = $('#Unshipped').prop('checked') ? "/unshipped" : "";
         $.getJSON({
-            url: "../../api/order" + id + "/" + unshipped,
+            url: "../../api/order/" + id + unshipped,
             success: function (response, textStatus, jqXhr) {
                 //console.log(response);
+                var empty, css;
                 $('#order_rows').html("");
-                for (var i = 0; i < response.length; i++) { 
-                    var empty, css;
+                for (var i = 0; i < response.length; i++) {
                     if (response[i].shippedDate == null) {
-                        empty = "None";
-                        css = "class=\"unshipped\""
+                        empty = "Not shipped";
+                        var time = new Date();
+                        console.log(time);
+                        if (time > Date.parse(response[i].requiredDate.toString().substring(0, 10))) {
+                            css = "class=\"unshipped\"";
+                        }
+                        else {
+                            css = "class=\"overdue\"";
+                        }
                     }
                     else {
-                        empty = response[i].shippedDate;
+                        empty = response[i].shippedDate.toString().substring(0, 10);
                         css = "";
                     }
-                    var row = "<tr" + css + " data-id=\"" + response[i].orderId + "\" data-order-date=\"" + response[i].orderDate + "\" data-required-date=\"" + response[i].requiredDate + "\" data-shipped-date=\"" + response[i].shippedDate + "\">"
-                        + "<td class=\"text-right\">" + "</td>"
-                        + "<td class=\"text-right\">" + response[i].orderId + "</td>"
-                        + "<td class=\"text-right\">" + response[i].orderDate + "</td>"
-                        + "<td class=\"text-right\">" + response[i].requiredDate + "</td>"
+                    var row = "<tr " + css + ">"
+                        + "<td></td>"
+                        + "<td class=\"text-left\">" + response[i].orderId + "</td>"
+                        + "<td class=\"text-right\">" + response[i].orderDate.toString().substring(0, 10) + "</td>"
+                        + "<td class=\"text-right\">" + response[i].requiredDate.toString().substring(0, 10) + "</td>"
                         + "<td class=\"text-right\">" + empty + "</td>"
                         + "</tr>";
                     $('#order_rows').append(row);
@@ -44,40 +44,10 @@ $(function () {
     }
 
     $('#Unshipped').on('change', function () {
-        $('#order_rows').data('id', $(this).val());
         getOrders();
     });
-});
-*/
 
-$(function () {
-    getOrders()
-
-    function getOrders() {
-        var id = $('#order_rows').data('id');
-        var unshipped = $('#Unshipped').prop('checked') ? "" : "/unshipped";
-        $.getJSON({
-            url: "../../api/order/" + id + "/" + unshipped,
-            success: function (response, textStatus, jqXhr) {
-                //console.log(response);
-                $('#order_rows').html("");
-                for (var i = 0; i < response.length; i++) {
-                    var row = "<tr>"
-                        + "<td>" + response[i].orderId + "</td>"
-                        + "<td class=\"text-right\">$" + response[i].orderDate + "</td>"
-                        + "<td class=\"text-right\">" + response[i].requiredDate + "</td>"
-                        + "</tr>";
-                    $('#order_rows').append(row);
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                // log the error to the console
-                console.log("The following error occured: " + textStatus, errorThrown);
-            }
-        });
-    }
-
-    $('#Unshipped').on('change', function () {
-        getOrders();
-    });
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
 });
